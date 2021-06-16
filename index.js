39bd367dd4429b4f1430a7976e35e02e0ef896a7
ColinAryee <46033005+ColinAryee@users.noreply.github.com>
2021-06-16 11:21:12 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const rollDice = () => {
+    return Math.floor(Math.random() * 6) + 1;
+}
+
+let randomNumber1  = rollDice(), randomNumber2  = rollDice();
+
+
+const displayDIceRoll = (img, diceVal) => {
+    document.querySelector(img).setAttribute("src", `./images/dice${diceVal}.png`);
+}
+
+const getWinner = (roll1, roll2) => {
+    if (roll1 == roll2) {
+        return "DRAW"
+    }
+    return (roll1 > roll2) ? 1 : 2;
+}
+
+const displayWinner = () => {
+    const draw = "DRAW"
+    const winner = getWinner(randomNumber1, randomNumber2);
+    const winnerText = document.querySelector("h1");
+    (winner == draw) ? winnerText.innerText=draw : winnerText.innerText=`WINNER: PLAYER ${winner}`
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    displayDIceRoll(".img1", randomNumber1);
+    displayDIceRoll(".img2", randomNumber2);
+
+    displayWinner(); 
+})
+
